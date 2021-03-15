0ca7307d908a11db721f0ce75c5919cea20d8888
Luke Phillips <phillipsld@hotmail.co.uk>
2021-03-15 13:24:23 +0000

    Add sotck page and initialising VUE

---
+++ src/pages/AddStock.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row">
+    <div class="col-md-8">
+      <edit-stock-form :model="model">
+      </edit-stock-form>
+    </div>
+  </div>
+</template>
+<script>
+  import EditStockForm from './Stocks/EditStockForm'
+  export default {
+    components: {
+      EditStockForm,
+    },
+    data() {
+      return {
+        model: {
+          companyName: 'Legal & General',
+          tickerSymbol: 'LGEN'
+        }
+      }
+    }
+  }
+</script>
+<style>
+</style>
---
+++ src/pages/Stocks/EditStockForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <card>
+    <h5 slot="header" class="title">Add Stock</h5>
+    <div class="row">
+      <div class="col-md-3 px-md-1">
+        <base-input label="Company Name"
+                  placeholder="Company name"
+                  v-model="model.companyName">
+        </base-input>
+      </div>
+      <div class="col-md-5 pr-md-1">
+        <base-input label="Ticker Symbol"
+                  placeholder="Ticker symbol"
+                  v-model="model.tickerSymbol">
+        </base-input>
+      </div>
+    </div>
+    <base-button slot="footer" type="primary" fill>Save</base-button>
+  </card>
+</template>
+<script>
+  export default {
+    props: {
+      model: {
+        type: Object,
+        default: () => {
+          return {};
+        }
+      }
+    }
+  }
+</script>
+<style>
+</style>
