acf418463307ccd1fda5aaa1c64df9f51b9c3b01
Luke Phillips <phillipsld@hotmail.co.uk>
2022-03-22 11:14:05 +0000

    salary line chart

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using Azure.Cosmos;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+const string EndpointUrl = "https://personal-finance-db.documents.azure.com:443/";
+const string AuthorizationKey = "uKehVT4myAIG69BAYyLZOzHlxLh4Wx0JotaD0OQeg54lrcsWR8vQLpkAnfIKCv0j6Cd5hSCco26oyD9pQFbgwA==";
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new CosmosClient(EndpointUrl, AuthorizationKey));
 
 await builder.Build().RunAsync();
+
+CosmosClient cosmosClient = new CosmosClient(EndpointUrl, AuthorizationKey);
+//await Budget.AddItemsToContainerAsync(cosmosClient);
