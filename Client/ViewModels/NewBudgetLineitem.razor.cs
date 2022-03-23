1a1c13b09ab1e0b0f9d32ef01bd19435e5ddf308
Luke Phillips <phillipsld@hotmail.co.uk>
2022-03-23 09:25:44 +0000

    bound dropdown fields

--- Client/ViewModels/NewBudgetLineitem.razor.cs
+++ Client/ViewModels/NewBudgetLineitem.razor.cs
@@ -3,14 +3,17 @@ using Microsoft.JSInterop;
 
 namespace Cheddar.Client.ViewModels {
     public class BudgetLineItemVM {
-
-        BudgetLineItemModel blm = new BudgetLineItemModel();
         public string BudgetLineName { get; set; }
-
+        public double Cost { get; set; }
+        public string Category { get; set; }
+        public string PaymentMethod { get; set; }
+        public string ContractEndDate { get; set; }
         public BudgetLineItemVM(BudgetLineItemModel bliModel)
         {
             this.BudgetLineName = bliModel.BudgetLineName;
-            Console.WriteLine(blm.BudgetLineName);       
+            this.Category = bliModel.Category;
+            this.PaymentMethod = bliModel.PaymentMethod;
+            this.ContractEndDate = bliModel.ContractEndDate;
         }
     }
 }
