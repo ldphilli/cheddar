a41ea1ef334c093b4e278c3374a7ab4c083fa104
Luke Phillips <phillipsld@hotmail.co.uk>
2022-03-30 16:33:09 +0100

    create new budget line item

--- Client/ViewModels/Budget.razor.cs
+++ Client/ViewModels/Budget.razor.cs
@@ -2,61 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Net;
+using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Azure.Cosmos;
+//using Newtonsoft.Json;
+using Cheddar.Client.Models;
 
 namespace Cheddar.Client.ViewModels {
-    public partial class Budget {
-        private const string EndpointUrl = "https://personal-finance-db.documents.azure.com:443/";
-        private const string AuthorizationKey = "uKehVT4myAIG69BAYyLZOzHlxLh4Wx0JotaD0OQeg54lrcsWR8vQLpkAnfIKCv0j6Cd5hSCco26oyD9pQFbgwA==";
-        private const string DatabaseId = "Cheddar";
-        private const string ContainerId = "BudgetLineItems";
+    public class BudgetVM {
 
-        /*static async Task Main(string[] args)
-        {
-            CosmosClient cosmosClient = new CosmosClient(EndpointUrl, AuthorizationKey);
-            await Budget.AddItemsToContainerAsync(cosmosClient);
-        }
         /// <summary>
         /// Add BudgetLineItem items to the container
         /// </summary>
-        public static async Task AddItemsToContainerAsync(CosmosClient cosmosClient)
-        {
-
-            const string DatabaseId = "Cheddar";
-            const string ContainerId = "BudgetLineItems";
-            // Create a BudgetLineItem object for the Andersen BudgetLineItem
-            BudgetLineItem newBudgetLineItem = new BudgetLineItem
-            {
-                Id = "2",
-                BudgetLineName = "Adobe Creative Cloud",
-                Cost = 16.24,
-                Category = "Enteratinment",
-                PaymentMethod = "Paypal",
-                ContractEndDate = "Rolling month"
-            };
-
-            CosmosContainer container = cosmosClient.GetContainer(DatabaseId, ContainerId);
-            try
-            {
-
-                // Read the item to see if it exists.  
-                ItemResponse<BudgetLineItem> newBudgetLineItemResponse = await container.ReadItemAsync<BudgetLineItem>(newBudgetLineItem.Id, new Azure.Cosmos.PartitionKey(newBudgetLineItem.Id));
-                Console.WriteLine("Item in database with id: {0} already exists\n", newBudgetLineItemResponse.Value.Id);
-            }
-            catch(CosmosException ex) when (ex.Status == (int)HttpStatusCode.NotFound)
-            {
-                // Create an item in the container representing the Andersen family. Note we provide the value of the partition key for this item, which is "Andersen"
-                ItemResponse<BudgetLineItem> newBudgetLineItemResponse = await container.CreateItemAsync<BudgetLineItem>(newBudgetLineItem, new Azure.Cosmos.PartitionKey(newBudgetLineItem.Id));
-
-                // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse.
-                Console.WriteLine("Created item in database with id: {0}\n", newBudgetLineItemResponse.Value.Id);
-            }
-        }*/
+        public async Task AddItemsToContainerAsync(BudgetLineItemModel budgetLineItem) {
+            HttpyClient client = new HttpClient();
+            var url = "http://localhost:7071/api/CreateBudgetLineItem";
+            await client.PostAsJsonAsync(url, budgetLineItem);
+        }
     }
 }
 
