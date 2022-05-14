9dc34a24933ea9b4e1f2cb2fa4725577dfbaa9c2
Luke Phillips <luke@ldphilli.co.uk>
2022-05-14 18:58:54 +0100

    Azure B2C Login

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
 using Cheddar.Client.Services;
+using Cheddar.Client.Shared;
 using Cheddar.Client.ViewModels;
 using Cheddar.Shared.Models;
 
@@ -17,4 +19,8 @@ builder.Services.AddTransient<BudgetVM>();
 builder.Services.AddTransient<BudgetSettingsService>();
 //Add application state model as a singleton
 builder.Services.AddSingleton<ApplicationState>();
+builder.Services.AddMsalAuthentication(options => {
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://CheddarApp.onmicrosoft.com/25b6b977-0a24-4ba7-9795-45437c5cff10/Api.ReadWrite");
+});
 await builder.Build().RunAsync();
