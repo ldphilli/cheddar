7ba4bc278bf49d666d094b098791b0d1608f51f7
Tom Ashworth <thomas.d.ashworth@gmail.com>
2022-06-14 20:47:59 +0000

    support no `API_Prefix`

--- Client/Program.cs
+++ Client/Program.cs
@@ -22,15 +22,15 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 //         builder.Configuration["API_Prefix"]
 //         }));
 
-builder.Services.AddHttpClient("WebAPI", 
-        client => client.BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler(sp => sp.GetRequiredService<AuthorizationMessageHandler>()
-     .ConfigureHandler(authorizedUrls: new [] { 
-         builder.Configuration["API_Prefix"]
-         }));
-         
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
-    .CreateClient("WebAPI"))
+var baseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress);
+//Console.WriteLine("Base Address: " + baseAddress);
+
+builder.Services
+    .AddHttpClient("WebAPI", client => client.BaseAddress = baseAddress)
+    .AddHttpMessageHandler(sp => sp.GetRequiredService<AuthorizationMessageHandler>().ConfigureHandler(authorizedUrls: new[] { baseAddress.ToString() }));
+
+builder.Services
+    .AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("WebAPI"))
     .AddSingleton<ApplicationState>();
 
 builder.Services.AddTransient<BudgetSettingsService>();
@@ -45,12 +45,12 @@ builder.Services.AddTransient<AuthenticationService>();
 builder.Services.AddSingleton<ApplicationState>();
 builder.Services.AddMsalAuthentication(options =>
 {
-    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
-    //options.ProviderOptions.DefaultAccessTokenScopes.Add("https://CheddarApp.onmicrosoft.com/25b6b977-0a24-4ba7-9795-45437c5cff10/Api.ReadWrite");
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("e740fe9f-b317-4aa5-8196-9be58e0b30e3");
-    options.AuthenticationPaths.LogOutSucceededPath = "/";
+  builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+  //options.ProviderOptions.DefaultAccessTokenScopes.Add("https://CheddarApp.onmicrosoft.com/25b6b977-0a24-4ba7-9795-45437c5cff10/Api.ReadWrite");
+  options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+  options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+  options.ProviderOptions.DefaultAccessTokenScopes.Add("e740fe9f-b317-4aa5-8196-9be58e0b30e3");
+  options.AuthenticationPaths.LogOutSucceededPath = "/";
 });
 
 await builder.Build().RunAsync();
