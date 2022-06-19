a39abb535825fd5302426730a69d31cdae1385a3
ldphilli <phillipsld@hotmail.co.uk>
2022-06-19 15:37:41 +0100

    Update MonthlyBudgetService.cs

--- Client/Services/MonthlyBudgetService.cs
+++ Client/Services/MonthlyBudgetService.cs
@@ -7,17 +7,15 @@ namespace Cheddar.Client.Services {
     public class MonthlyBudgetService {
 
         private readonly HttpClient ApiClient;
-        private ApplicationState appState;
 
-        public MonthlyBudgetService(HttpClient apiClient, ApplicationState applicationState)
+        public MonthlyBudgetService(HttpClient apiClient)
         {
             ApiClient = apiClient;
-            appState = applicationState;
         }
 
         public async Task<MonthlyBudgetModel> GetMonthlyBudget(int month, int year) {
             Console.WriteLine("Entered into monthly budget service GetMonthlyBudget");
-            string request = String.Concat("api/GetMonthlyIncome?claim=", appState.Token);
+            string request = String.Concat("api/GetMonthlyIncome");
             return await ApiClient.GetFromJsonAsync<MonthlyBudgetModel>(request);
         }
     }
