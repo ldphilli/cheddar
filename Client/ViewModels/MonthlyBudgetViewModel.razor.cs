55921c6ef56743189762d6ce4924aa32634cca87
Luke Phillips <phillipsld@hotmail.co.uk>
2022-12-22 15:15:02 +0000

    Can select year and month of budget to view

--- Client/ViewModels/MonthlyBudgetViewModel.razor.cs
+++ Client/ViewModels/MonthlyBudgetViewModel.razor.cs
@@ -1,6 +1,7 @@
 using Cheddar.Shared.Models;
 using Cheddar.Client.Services;
 using Microsoft.AspNetCore.Components;
+using System.Globalization;
 
 namespace Cheddar.Client.ViewModels {
     public class MonthlyBudgetViewModel {
@@ -10,6 +11,10 @@ namespace Cheddar.Client.ViewModels {
         public MonthlyBudgetModel monthlyBudgetModel => appState.monthlyBudgetModel;
         public MonthlyBudgetService monthlyBudgetService;
         private readonly NavigationManager nvm;
+        public List<int> years = new List<int>();
+        public List<MonthModel> monthsForYear = new List<MonthModel>();
+        public int selectedYear;
+        public MonthModel selectedMonth;
 
         public MonthlyBudgetViewModel(HttpClient apiClient, NavigationManager navManager, ApplicationState applicationState, MonthlyBudgetService mbService)
         {
@@ -36,10 +41,37 @@ namespace Cheddar.Client.ViewModels {
 
         public async Task GetAvailableYearsForUser() {
 
+            List<MonthlyBudgetModel> allMonthlyBudgetsForUser = await monthlyBudgetService.GetAllMonthlyBudgetDatesForUser();
+            years = allMonthlyBudgetsForUser.OrderBy(x => x.Year)
+            .Select(x => x.Year)
+            .Distinct()
+            .ToList();
+            
+            selectedYear = years.LastOrDefault();
+
+            var months = allMonthlyBudgetsForUser.Where(x => x.Year == years.FirstOrDefault())
+            .OrderBy(x => x.Month)
+            .Select(x => x.Month)
+            .Distinct()
+            .ToList();
+
+            months.ForEach(delegate(int month) {
+                monthsForYear.Add(
+                    new MonthModel {
+                        MonthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month),
+                        MonthNumber = month
+                    }
+                    //monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month)
+                );           
+            });
+            
+            selectedMonth = monthsForYear.LastOrDefault();
+
         }
 
-        public async Task GetAvailableMonthsForSelectedYear() {
+        public async Task GetSpecificMonthlyBudget() {
 
+            appState.monthlyBudgetModel = await monthlyBudgetService.GetMonthlyBudget(selectedMonth.MonthNumber, selectedYear);
         }
     }
 }
---
+++ Shared/Models/MonthModel.cs
@@ -0,0 +1,6 @@
+namespace Cheddar.Shared.Models {
+    public class MonthModel {
+        public int MonthNumber { get; set; }
+        public string? MonthName { get; set; }
+    }
+}
